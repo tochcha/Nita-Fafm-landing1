fdee11f2e8913935f45db7c74990769c05a17b22
alex <tochcha@gmail.com>
2019-06-23 13:25:44 +0300

    finish

--- app/js/custom.js
+++ app/js/custom.js
@@ -267,6 +267,22 @@ $('.subtabs li span').click(function () {
 /* tabs*/
 
 
+$(window).load(function(){
+	
+	/* Page Scroll to id fn call */
+	$("#navigation-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+		highlightSelector:"#navigation-menu a"
+	});
+	
+	/* demo functions */
+	$("a[rel='next']").click(function(e){
+		e.preventDefault();
+		var to=$(this).parent().parent("section").next().attr("id");
+		$.mPageScroll2id("scrollTo",to);
+	});
+	
+});
+
 /*$(window).bind('load', handler);
 $(window).bind('resize', handler);*/
 
